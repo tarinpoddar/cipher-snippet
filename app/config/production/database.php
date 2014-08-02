011e2ffa049736a4730c7829b52f846a320a86cd
Tarin Poddar <tarinpoddar27@gmail.com>
2014-08-02 16:08:09 -0400

    database production files added

---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => '127.9.208.2',
+            'database'  => 'ciphersnippet',
+            'username'  => 'adminMy8MqbC',
+            'password'  => 'm2UQxx1NH2fl',
+        ),
+
+    ),
+
+);
